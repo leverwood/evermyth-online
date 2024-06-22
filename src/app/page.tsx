4787e4aae92e71634ea1cc603326426d583f232d
leverwood <lilia.everwood@gmail.com>
2024-06-22 11:27:10 -0400

    Implement example from Auth 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Col, Row } from "react-bootstrap";
 
 import styles from "./page.module.scss";
---
+++ src/components/NavBarItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const NavBarItem = ({ children, href, className, icon, tabIndex, testId }) => {
+  const pathname = usePathname();
+  const activeClass = "navbar-item-active";
+  const activeClasses = className ? `${className} ${activeClass}` : activeClass;
+
+  return (
+    <span className="d-inline-flex align-items-center navbar-item">
+      <span
+        className={pathname === href ? activeClasses : className}
+        tabIndex={tabIndex}
+        data-testid={testId}
+      >
+        {children}
+      </span>
+    </span>
+  );
+};
+
+export default NavBarItem;
